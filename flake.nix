ea5e3c34b801c39f7f177048284d943b95a1ce99
Guillaume Maudoux <guillaume.maudoux@tweag.io>
2026-03-27 10:36:39 +0100

    Setup Nix environment
    Add flake.nix providing a devShell with bazel_8, nix, git, cacert.
    Track nixpkgs nixos-24.11 branch.
    Add nixpkgs.nix for rules_nixpkgs to resolve nixpkgs from flake.lock.
    Add .envrc for direnv integration.

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShellNoCC {
+          name = "abseil-cpp-nix-shell";
+          packages = with pkgs; [ bazel_8 bazel-buildtools cacert nix git ];
+        };
+      });
+}
